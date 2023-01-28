2ae68af58af8d74002678f64bbaf699fce7a0b7a
djdw2657 <djdw2657@gmail.com>
2023-01-28 17:37:16 +0900

    Instagram

---
+++ app/src/main/java/com/example/myinstagram/main/BottomNavigationMenu.kt
@@ -0,0 +1,54 @@
+package com.example.myinstagram.main
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.example.myinstagram.DestinationScreen
+
+enum class BottomNavigationItem(val icon: Int, val navDestination: DestinationScreen) {
+    FEED(com.example.myinstagram.R.drawable.ic_home, DestinationScreen.Feed),
+    SEARCH(com.example.myinstagram.R.drawable.ic_search, DestinationScreen.Search),
+    POSTS(com.example.myinstagram.R.drawable.ic_posts, DestinationScreen.MyPosts)
+}
+
+@Composable
+fun BottomNavigationMenu(selectedItem: BottomNavigationItem, navController: NavController) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentHeight()
+            .padding(top = 4.dp)
+            .background(Color.White)
+    ) {
+        for (item in BottomNavigationItem.values()) {
+            Image(
+                painter = painterResource(id = item.icon),
+                contentDescription = null,
+                modifier = Modifier
+                    .size(40.dp)
+                    .padding(5.dp)
+                    .weight(1f)
+                    .clickable {
+                        navigateTo(navController, item.navDestination)
+                    },
+                colorFilter = if (item == selectedItem) ColorFilter.tint(Color.Black)
+                else ColorFilter.tint(Color.Gray)
+            )
+        }
+    }
+}
+
+
+
+
+
+
+
